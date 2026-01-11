da967fea1079245ddb20846bd48f2105913b92da
Dev Ekkx <devekkx@gmail.com>
2026-01-11 10:39:26 +0000

    feature: implement looops

--- src/Loops.kt
+++ src/Loops.kt
@@ -1,3 +1,44 @@
-fun Loop() {
+fun loops() {
+//   println("How many numbers will you enter?")
+//   val amountOfNumbers = readln().toIntOrNull() ?: 0
 
+    /*
+   var sum = 0
+   var i = 0
+   while (i < amountOfNumbers) {
+ println("Enter number ${i + 1}:")
+      val num = readln().toIntOrNull() ?: continue
+      sum += num
+      i++
+   }
+
+   println("The total sum is $sum")
+**/
+
+    /*
+val numbers = mutableListOf<Int>()
+    for (i in 0..<amountOfNumbers) {
+        println("Enter number ${i + 1}:")
+        val num = readln().toIntOrNull() ?: continue
+        numbers.add(num)
+    }
+
+    for (number in numbers) {
+        println("You entered: $number")
+    }
+
+    **/
+
+//    String loop
+    println("Enter a string:")
+    val input = readln()
+
+//    Build reversal string
+    val finalString  = buildString {
+        for (i in input.lastIndex downTo 0) {
+            append(input[i])
+        }
+    }
+
+    println(finalString)
 }
